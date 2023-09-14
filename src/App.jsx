78cea3cc19a564961bc702b0d937725b2990569b
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-09-14 20:49:32 +0600

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Courses from './components/Courses/Courses'
+import Header from './components/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Header></Header>
+     <Courses></Courses>
+    </>
+  )
+}
+
+export default App
